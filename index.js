b301644c1c55b94aa223f600657446e9e90d9877
Fabien O'Carroll <fabien@allou.is>
2014-03-22 00:44:19 +0000

    use merge to implment defaults closes #1

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var Obstruct = require('obstruct');
 var minivents = require('minivents');
 var createGetter = require('./lib/createGet');
 var createSetter = require('./lib/createSet');
+var merge = require('./lib/merge');
 var noop = function () {};
 
 var Model = Obstruct.extend({
   constructor: function (data) {
     minivents(this);
-    data = data || {};
+    data = merge({}, this.defaults, data || {});
     var _attr = {};
     for (var prop in data) {
       if (data.hasOwnProperty(prop)) {
